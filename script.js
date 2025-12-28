c5e103c1fc122c133d4169500752bc4249292545
Damir-Fullstack <ortukovdamir773@gmail.com>
2025-12-28 12:06:09 +0100

    Improve background color toggle functionality
    Refactor background color toggle logic and update messages.

--- script.js
+++ script.js
@@ -4,23 +4,20 @@ const change =
   document.getElementById("c");
 const result =
   document.getElementById("b");
+const day = "#0000ff";
+  const night = "#000000";
 let isClickedChange = false;
-let b = document.body.style.backgroundColor === night;
-let a = isClickedChange && b;
 check.addEventListener("click", () => {
-  if(a) {
+  if(isClickedChange) {
     result.innerText = "Now is night!";
   } else {
     result.innerText = "Now is day!";
-  };
-});;
+  }
+  });
 change.addEventListener("click", () => {
   isClickedChange = !isClickedChange;
 if(isClickedChange){  document.body.style.backgroundColor = night;
      } else {
   document.body.style.backgroundColor = day;
-     };
-  const day = "#0000ff";
-  const night = "#000000";
-  };
+     }
 });
